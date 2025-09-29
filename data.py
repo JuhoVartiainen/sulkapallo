a5e035a5945490e59a07c846e6dce8b062d341d0
Juho Vartiainen <juhovartiainen05@gmail.com>
2025-09-29 18:56:27 +0300

    Lisättiin sopiville viikonpäiville oma parametri sopivat_viikonpäivät

--- data.py
+++ data.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 
 
-def scrape_data(sopivat_tunnit = [i for i in range(25)], week=0)-> list['Ma 29.9.20:00 Sulkapallo']: #0 = tämä viikko 1 = seuraava viikko
+def scrape_data(sopivat_viikonpäivät= ["ma, ti, ke, to, pe, la, su"], sopivat_tunnit = [i for i in range(25)], week=0): #0 = tämä viikko 1 = seuraava viikko #-> list['Ma 29.9.20:00 Sulkapallo']
     
     url = f"https://www.tuni.fi/sportuni/omasivu/?page=selection&lang=fi&type=3&area=2&week={week}"
 
@@ -12,7 +12,7 @@ def scrape_data(sopivat_tunnit = [i for i in range(25)], week=0)-> list['Ma 29.9
 
     date_link = []
     date_list = []
-    suitable_hours = [] #sopivien tuntien lista
+    suitable = [] #sopivien tuntien lista
 
     for a in soup.select("li a"):
         teksti = a.get_text(strip=True)
@@ -28,14 +28,14 @@ def scrape_data(sopivat_tunnit = [i for i in range(25)], week=0)-> list['Ma 29.9
         päivä_nr, kuukausi_nr, kellonaika = aika.split(".")
         tunnit, minuutit = kellonaika.split(":")
 
-        if int(tunnit) in sopivat_tunnit:
-            suitable_hours.append(date)
+        if int(tunnit) in sopivat_tunnit and viikonpäivä.lower() in sopivat_viikonpäivät:
+            suitable.append(date)
 
 
-    return suitable_hours
+    return suitable
 
 #base_url = "https://www.tuni.fi/sportuni/omasivu/"
 # full_url = base_url + linkki
 
 
-print(scrape_data(sopivat_tunnit=[20]))
+print(scrape_data(sopivat_viikonpäivät=["ma", "ke", "pe"], sopivat_tunnit=[12]))
